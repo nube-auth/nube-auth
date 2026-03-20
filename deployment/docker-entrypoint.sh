bdfc5fa38aeef4572caba3239f10054fdc3841b8
Devendra Pratap Singh <dps.manit@gmail.com>
2026-03-20 17:31:23 +0530

    fix: update nginx entrypoint and subdomain routing for core and workers services

--- deployment/docker-entrypoint.sh
+++ deployment/docker-entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 # Nube Auth – nginx entrypoint
-# Injects DOMAIN and GATEWAY_INTERNAL_URL into the nginx config template at startup,
-# then hands off to the standard nginx process.
+# Injects DOMAIN, GATEWAY_INTERNAL_URL, CORE_INTERNAL_URL, and WORKERS_INTERNAL_URL
+# into the nginx config template at startup, then hands off to the standard nginx process.
 set -e
 
 : "${DOMAIN:?DOMAIN environment variable is required (e.g. DOMAIN=nubeauth.com)}"
 : "${GATEWAY_INTERNAL_URL:?GATEWAY_INTERNAL_URL environment variable is required (e.g. GATEWAY_INTERNAL_URL=http://nubeauth-gateway.railway.internal:8080)}"
+: "${CORE_INTERNAL_URL:?CORE_INTERNAL_URL environment variable is required (e.g. CORE_INTERNAL_URL=http://nubeauth-core.railway.internal:8080)}"
+: "${WORKERS_INTERNAL_URL:?WORKERS_INTERNAL_URL environment variable is required (e.g. WORKERS_INTERNAL_URL=http://nubeauth-workers.railway.internal:8080)}"
 
-envsubst '${DOMAIN} ${GATEWAY_INTERNAL_URL}' \
+envsubst '${DOMAIN} ${GATEWAY_INTERNAL_URL} ${CORE_INTERNAL_URL} ${WORKERS_INTERNAL_URL}' \
   < /etc/nginx/templates/subdomains.conf.template \
   > /etc/nginx/conf.d/default.conf
 
